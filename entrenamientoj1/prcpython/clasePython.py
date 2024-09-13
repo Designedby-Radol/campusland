d11dbf0509256c9b181d4e65b5a0e5c00b45fcae
KyokoToreno <kyokotoreno@proton.me>
2024-09-13 12:57:14 -0500

    Bro unos consejitos

--- entrenamientoj1/prcpython/clasePython.py
+++ entrenamientoj1/prcpython/clasePython.py
@@ -16,19 +16,19 @@ print('a * b = ', producto)
 print('a / b = ', division)
 print('a % b = ', mod)
 print('a // b = ', entera)
-print('a ** b = ', exponencial,'\n','\n')
+print('a ** b = ', exponencial ,'\n\n') # No seas manco bro, no necesitas separar los \n
 
 nombre = str('Raul')
-print (f'el nombre ke ingreso es "{nombre}"','\n','\n')
+print (f'el nombre ke ingreso es "{nombre}"', '\n\n')
 
-print (f'nombre: \n{nombre}','\n')
-print ("el nombre que ingreso es","holaa","si",sep = '\n')
+print (f'nombre: \n{nombre}\n')
+print ('el nombre que ingreso es', 'holaa' , 'si', sep = '\n') # Tienes q ser consistente, o vas a usar ' o vas a usar ", no uses los dos al azar
 print ('\n')
-print (f"Fundamentos","Programacion","en",sep = '***',end = "...Python")
+print (f'Fundamentos', 'Programacion', 'en', sep = '***', end = '...Python') # espacios bro por favor, hay q poner espacios
 
 
-print(type(52))# int
-print(type(1.5))# float
-print(type(True))#  boolean
-print(type("hola mondu"))#  string
-print(type(5 + 2j))# complex number
+print(type(52)) # int
+print(type(1.5)) # float
+print(type(True)) # boolean
+print(type('hola mondu')) # string
+print(type(5 + 2j)) # complex number
